6d6618e5316886829ef92628aa1c289c2ed1b6e9
João Henrique - Linux <joaohenriquelopesdiv@gmail.com>
2025-02-13 14:18:02 -0300

    Feat: Lists

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,83 @@
 import React from "react";
-import Artist from '../components/Artist'
-import Song from '../components/Song'
-import '../styles/Main.css'
+
+// Components
+
+import Artist from "../components/Artist";
+import Song from "../components/Song";
+import Album from "../components/Album";
+
+// Styles
+
+import "../styles/Main.css";
+
+// Data Base
+
+import artistsArray from "../database/artists.json";
+import songsArray from "../database/songs.json";
+import albumsArray from "../database/albums.json";
+
+
 
 const Main = () => {
-
   return (
     <main className="main">
-        <div className="musician-list">
-            <div className="musician-list__header">
-                <h2 className="">Artistas populares</h2>
-                <a className="musician-list_link" href="/">Mostrar mais</a >
-            </div>
-            <div>
-                <Artist />
-                <Song />
-            </div>
+      <div className="musician-list">
+        <div className="musician-list__header">
+          <h2 className="">Artistas populares</h2>
+          <a className="musician-list__link" href="/">
+            Mostrar tudo
+          </a>
         </div>
+        <div className="musician-list__list">
+          {artistsArray.map((artist, index)=>(
+            <Artist
+            key={index}
+            name={artist.name}
+            image={artist.image}
+            type={artist.type}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="song-list">
+        <div className="song-list__header">
+          <h2 className="">Ouvir denovo</h2>
+          <a className="song-list__link" href="/">
+            Mostrar tudo
+          </a>
+        </div>
+        <div className="song-list__list">
+        {songsArray.map((song, index)=>(
+            <Song
+            key={index}
+            name={song.name}
+            artist={song.artist}
+            image={song.image}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="album-list">
+        <div className="album-list__header">
+          <h2 className="">Albums</h2>
+          <a className="album-list__link" href="/">
+            Mostrar tudo
+          </a>
+        </div>
+        <div className="album-list__list">
+        {albumsArray.map((album, index)=>(
+            <Album
+            key={index}
+            name={album.name}
+            year={album.year}
+            artist={album.artist}
+            image={album.image}
+            />
+          ))}
+        </div>
+      </div>
     </main>
-)
-
-}
+  );
+};
 
 export default Main;
