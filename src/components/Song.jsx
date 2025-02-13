0feef1cb2948fdb3ce1e09b37cd7973ce4fc2b75
João Henrique - Linux <joaohenriquelopesdiv@gmail.com>
2025-02-13 14:11:19 -0300

    Feat: Song Component, Style and JSON

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,25 +1,20 @@
 import React from "react";
-import songsArray from "../database/songs";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 import "../styles/Song.css";
 
-const Song = () => {
-  const handleClick = () => {
-    console.table(songsArray);
-  };
-
+const Song = ({name, artist, image}) => {
   return (
     <div className="song-card">
       <FontAwesomeIcon className="song-card__play" icon={faCirclePlay} />
       <img
         className="song-card__image"
-        src={`${songsArray[0].image}`}
-        alt={`Songa: ${songsArray[0].name}`}
+        src={`${image}`}
+        alt={`Song: ${name}`}
       />
       <div className="song-card__heading">
-        <h2 className="song-card__heading__name">{songsArray[0].song}</h2>
-        <h3 className="song-card__heading__type">{songsArray[0].name}</h3>
+        <h2 className="song-card__heading__name">{name}</h2>
+        <h3 className="song-card__heading__type">{artist}</h3>
       </div>
     </div>
   );
